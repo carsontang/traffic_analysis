5c1b1730c4256c28b9e3e8988049e0e6fd0daead
Carson Tang <tang.carson@gmail.com>
2011-12-01 17:22:29 -0800

    Finished script.

---
+++ analyzer.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python
+
+import csv
+import sys
+
+THRESHOLD = 8.0
+DEFAULT_DURATION = 200.0
+
+csvFilename = sys.argv[1]
+outputFilename = sys.argv[2]
+outputFile = open(outputFilename, 'w')
+
+data = csv.reader(open(csvFilename, 'rb'))
+for row in data:
+	start_time = float(row[10])
+	duration = float(row[11])
+	if duration > 8:
+		duration = 200
+
+	outputFile.write(str(start_time) + "," + str(duration) + "\n")	
